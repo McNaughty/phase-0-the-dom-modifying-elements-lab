f60417b7ea9418bba2409480f02a024786695c47
Winnie <wimacoj@gmail.com>
2023-09-22 15:23:42 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+
+document.querySelector("main").remove();
+
+const newHeader = document.createElement("h1");
+document.body.append(newHeader); 
+
+newHeader.id = "victory";
+
+newHeader.id = "victory";
+
+newHeader.innerHTML = 'YOUR-NAME is the champion';
